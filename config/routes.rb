db13661aba53996c5939321c1b15a4f31d3c0dd7
davidmesquita <devid.mesquita13@gmail.com>
2025-09-04 18:59:31 -0300

    fix database.yml file and create cart routes

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+  resources :products
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  root "rails/health#show"
+   resource :cart, only: [:show] do
+    post 'add_item', to: 'carts#add_item'
+    delete ':product_id', to: 'carts#remove_item'
+  end
+end
